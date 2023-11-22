5540dbd6d50b51458f859fd03ac91aab4ca0a69b
jeonghwan15 <sdf118@naver.com>
2023-11-22 17:30:41 +0900

    1122

---
+++ 2023-11-22/static/javascript/array2.js
@@ -0,0 +1,54 @@
+
+// let num = [10,20,30,40,50,60];
+
+// // num 배열에 10단위로 100까지 숫자를 추가로 저장하세요
+// //num.push(70,80,90,100);
+// num.push(70);
+// num.push(80);
+// num.push(90);
+// num.push(100);
+// // num배열에 저장된 숫자를 한줄에 하나씩 출력하세요(필! 반복문사용)
+// for( var i=0; i<num.length;i++){
+//     document.write( num[i]+"<br>");
+// }
+
+// // num배열에 저장된 숫자들에게 +3을 해주고 결과를 출력하세요
+// for( var i=0; i<num.length; i++){
+//     num[i] = num[i]+3;
+//     document.write( num[i] +" ");
+// }
+
+// // num 배열에서 55보다 큰 숫자들만 출력하세요
+// for ( var i=0; i<num.length; i++){
+//     if(num[i] > 55){
+//     document.write( num[i] + " ");
+//     }
+// }
+
+let num1 = [1,2,3];
+let num2 = [4,5,6];
+// 두 배열에 같은 인덱스위치에 있는 숫자들끼리의 합을 출력하세요
+// 결과 ->  5 7 9
+for( var i=0; i<num1.length; i++){
+    document.write((num1[i]+num2[i]) + " ");
+}
+
+// num1 과 num2 배열에서 짝수에 해당하는 숫자만
+// num3 이라는 배열에 저장하고 출력
+let num3 = new Array();
+for( var i=0; i<num1.length; i++){
+    if ( (num1[i])%2 == 0)
+        num3.push(num1[i]);
+    if( num2[i]%2 == 0)
+        num3.push(num2[i]);
+    }
+document.write( num3.sort().reverse() );
+
+
+// 과제!!!
+// 한화이글스 투수들의 평균자책점
+// name=["주현상","윤대경","이민우","이태양","페냐","박상원","장시환"]
+// era = [1.96 , 2.45 , 2.63 3.23 , 3.60 , 3.65 , 3.38 ]
+// era -> 평균자책점
+// 7명 선수들의 평균자책점의 평균치를 구하시오
+// 평균치 이상인 선수들이 누구인지 출력하세요
