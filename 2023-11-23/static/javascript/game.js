9d619dfb7703b34f4846f1bf09c0e55b0f8bbaf9
BuildTools <unconfigured@null.spigotmc.org>
2023-11-28 08:33:18 +0900

    1127

--- 2023-11-23/static/javascript/game.js
+++ 2023-11-23/static/javascript/game.js
@@ -34,5 +34,32 @@ $(function(){
 // 인덱스 -> 규칙 따라 반복문
 //.numBox 말고 board[idx]로만 , 변수필요
 function endgame(){
+    var row=0;
+    var col=0;
+    var cross=0;
 
+    for(i=0; i<board.length; i++){
+        var check = false;
+        for(var v=0; v<board[i].length;j++){
+            if(board[j][i]==0){
+                check = true;
+            }else{
+                check=false;
+                break;
+            }
+        }
+        if(check) col++;
+    }
+    var right = 0;
+    var left = 0;
+    for(i=0; i<board[0].length;i++){
+        if(board[i][i]==0) right++;
+        if(board[board.length-i-1] [i]==0) left++;
+        if(right==5 || left==5) cross++;
+    }
+    if(right==5 && left==5) cross++;
+
+    if((cross+row+col)==5){
+        
+    }
 }
