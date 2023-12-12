8121c3750f8b81bf332415d856d823b1456fa7ac
BuildTools <unconfigured@null.spigotmc.org>
2023-12-13 08:34:16 +0900

    1212

--- 2023-12/부루마블/static/javascript/board.js
+++ 2023-12/부루마블/static/javascript/board.js
@@ -1,33 +1,33 @@
 
-const zone_name=[
-    "복지기금","화성","성남","창원","제주","용인","수원","울산",
-    "인천공항","청주","광주","부천","대구","남양주","인천","포항",
-    "복지기금납부","천안","부산","전주","서울","안산","대전",
-    "무인도","안양","김해","평택","시흥","파주","의정부","김포","출발"
-];
-const zone_color=["#FF2424","#53C14B","#FFBB00","#5F00FF"]; // 각 면의 색상
-const land_purchase=[  // 각 도시의 매입 가격 ( 만단위)
-    0, 25, 27, 26, 34, 28, 27, 39,
-    0, 16, 42, 20, 45, 19, 48, 27, 
-    0, 21, 52, 20, 80, 22, 120,
-    0, 12, 10, 15, 12, 9, 9, 7, 0
+// const zone_name=[
+//     "복지기금","화성","성남","창원","제주","용인","수원","울산",
+//     "인천공항","청주","광주","부천","대구","남양주","인천","포항",
+//     "복지기금납부","천안","부산","전주","서울","안산","대전",
+//     "무인도","안양","김해","평택","시흥","파주","의정부","김포","출발"
+// ];
+// const zone_color=["#FF2424","#53C14B","#FFBB00","#5F00FF"]; // 각 면의 색상
+// const land_purchase=[  // 각 도시의 매입 가격 ( 만단위)
+//     0, 25, 27, 26, 34, 28, 27, 39,
+//     0, 16, 42, 20, 45, 19, 48, 27, 
+//     0, 21, 52, 20, 80, 22, 120,
+//     0, 12, 10, 15, 12, 9, 9, 7, 0
 
-];
+// ];
 
-const bg_image= [ // 모서리구역의 배경이미지
-    "출발.jpg", "무인도.jpg","복지기금.jpg","인천공항.jpg"
-];
+// const bg_image= [ // 모서리구역의 배경이미지
+//     "출발.jpg", "무인도.jpg","복지기금.jpg","인천공항.jpg"
+// ];
 
-// 각 구역의 객체 생성자 함수
-//  구역 이름, 토지매입가격, 소유자, 색상, 기능(모서리부분), 이미지
-function zone_Object( name, purchase, owner, color, func, image){
-    this.name=name;
-    this.purchase=purchase;
-    this.owner=owner;
-    this.color=color;
-    this.func=func;
-    this.back=image;
-}
+// // 각 구역의 객체 생성자 함수
+// //  구역 이름, 토지매입가격, 소유자, 색상, 기능(모서리부분), 이미지
+// function zone_Object( name, purchase, owner, color, func, image){
+//     this.name=name;
+//     this.purchase=purchase;
+//     this.owner=owner;
+//     this.color=color;
+//     this.func=func;
+//     this.back=image;
+// }
 
 //플레이어 생성자 함수
 function player(num, color){
@@ -110,7 +110,7 @@ function change_pcl(){
 
 }
 $(function(){
-    zone_create();
+    // zone_create();
     
     zone_draw();
 
